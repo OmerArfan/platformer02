6363868423db321cdaff22bce9a4b6b20f710bf8
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-01-17 22:46:34 +0500

    Version 1.3.0.1(17/01/2026)
    Hopefully the new favicon works without breaking.

--- platformer.py
+++ platformer.py
@@ -18,7 +18,7 @@ from datetime import datetime, date
 from bidi.algorithm import get_display
 
 # GAME VERSION
-version = "1.3.0"
+version = "1.3.0.1"
 
 # for compilation
 def resource_path(relative_path): 
