af7cd8f04b444fc3976520dd783152ca658f2498
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-06-06 20:39:17 +0500

    Updated build number.

--- cleobo/startup.py
+++ cleobo/startup.py
@@ -7,7 +7,7 @@ import traceback
 
 # Initializing Game and Engine Version
 manage_data.version = "1.3.9.0484"
-manage_data.kernel = "0.4.1.0032"
+manage_data.kernel = "0.4.1.0033"
 print(f"Game version {manage_data.version} (Powered by Cleobo {manage_data.kernel})")
 
 manage_data.now = datetime.now()
