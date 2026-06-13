a99a250fb9895407fb9167d1b2d431c6a7661eb0
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-06-13 12:26:56 +0500

    Roboquix Build 489 | Cleobo Build 38 {v0.5.0): Added speed changes for death, Mechanics Level 5 now working, and handling of bottom collisions.

--- cleobo/startup.py
+++ cleobo/startup.py
@@ -6,8 +6,8 @@ from datetime import datetime
 import traceback
 
 # Initializing Game and Engine Version
-manage_data.version = "1.3.9.0488"
-manage_data.kernel = "0.4.1.0037"
+manage_data.version = "1.3.9.0489"
+manage_data.kernel = "0.5.0.0038"
 print(f"Game version {manage_data.version} (Powered by Cleobo {manage_data.kernel})")
 
 manage_data.now = datetime.now()
