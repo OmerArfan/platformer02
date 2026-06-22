e56274b7484e9d0d4a2a605eb85965271b184391
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-06-22 08:09:18 +0500

    Added placeholder for desert.lua

---
+++ assets/data/levels/desert.lua
@@ -0,0 +1,11 @@
+local lvl1 = {
+    player = {
+	    x = 600, y = 200,
+	    spawn_x = 600, spawn_y = 200,
+	},
+	blocks = {
+	    {x = 600,  y = 450, w = 200, h = 50},
+	},
+    exit = {x = 1200, y = 0, w = 140, h = 180}, 
+    next_page = "desert_lvl2",
+}
