0b5d9481e7834b8afb9f414f1545eb9d0ee1753a
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-07-07 20:59:09 +0500

    Roboquix v1.4.0

--- cleobo/startup.py
+++ cleobo/startup.py
@@ -7,7 +7,7 @@ import traceback
 from random import random
 
 # Initializing Game and Engine Version
-manage_data.version = "1.3.9.0514"
+manage_data.version = "1.4.0.0516"
 manage_data.kernel = "0.7.2.0061"
 print(f"Game version {manage_data.version} (Powered by Cleobo {manage_data.kernel})")
 
