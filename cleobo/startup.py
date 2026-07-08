2f0181e442477181560e92669e8f91459276692f
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2026-07-08 14:50:18 +0500

    Update engine version

--- cleobo/startup.py
+++ cleobo/startup.py
@@ -8,7 +8,7 @@ from random import random
 
 # Initializing Game and Engine Version
 manage_data.version = "1.4.0.0516"
-manage_data.kernel = "0.7.2.0061"
+manage_data.kernel = "0.7.2.0062"
 print(f"Game version {manage_data.version} (Powered by Cleobo {manage_data.kernel})")
 
 # Random final message
